0ffcebdb8c329af00355b0173d7dec4e217cb5f4
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2018-03-15 08:50:51 -0700

    created SleepingBarber.java and updated readme and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ua-cs422-hw3
+Author: Jeremiah Hanson
+
 Third HomeWork program for cs422. Deals with the Sleeping Barber problem using java threads and monitors.
---
+++ src/SleepingBarber.java
@@ -0,0 +1,10 @@
+/*
+ * SleepingBarber.java
+ * Author: Jeremiah Hanson
+ * ----------------------------------------------------------------
+ * This is the main class for my sleeping barber solution
+ */
+
+public class SleepingBarber {
+
+}
